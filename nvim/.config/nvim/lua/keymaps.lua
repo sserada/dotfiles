3575200ee4f553047f6a488d5e7335146c948cc0
SouHirawata <hirawatasou@gmail.com>
2023-01-14 00:43:57 +0900

    :gem: Add keymap for esc

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,5 +2,9 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 local term_opt = { silent = true }
 
+-- esc
+keymap('i', 'jk', '<esc>', term_opt)
+keymap('i', 'kj', '<esc>', term_opt)
+
 -- Fern
 keymap("n", "<C-e>", ":Fern . -reveal=% -drawer<CR>", opts)
