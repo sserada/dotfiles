aa7fd03ca03efb79ef77536f3d9566a782771452
SouHirawata <hirawatasou@gmail.com>
2023-01-24 23:03:39 +0900

    :recycle: Fix the keymaps for luasnip

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -17,8 +17,8 @@ keymap('n', '<C-e>', ':NvimTreeToggle<CR>', opts)
 keymap('n', 'ff', ':Telescope find_files<CR>', opts)
 
 -- luasnip
-keymap('i', '<C-j>', 'require('luasnip').jump(1)<CR>', opts)
-keymap('s', '<C-j>', 'require('luasnip').jump(1)<CR>', opts)
-keymap('i', '<C-k>', 'require('luasnip').jump(-1)<CR>', opts)
-keymap('s', '<C-k>', 'require('luasnip').jump(-1)<CR>', opts)
+keymap('i', '<C-j>', 'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<C-j>"', { expr = true })
+keymap('s', '<C-j>', 'luasnip#expand_or_jumpable() ? "<Plug>luasnip-expand-or-jump" : "<C-j>"', { expr = true })
+keymap('i', '<C-k>', 'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<C-k>"', { expr = true })
+keymap('s', '<C-k>', 'luasnip#jumpable(-1) ? "<Plug>luasnip-jump-prev" : "<C-k>"', { expr = true })
 
