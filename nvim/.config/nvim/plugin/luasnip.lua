e76cc6c9db15094da3188db5a462b47be83952e1
SouHirawata <hirawatasou@gmail.com>
2023-01-24 22:57:42 +0900

    :gem: Add cpp snippets

--- nvim/.config/nvim/plugin/luasnip.lua
+++ nvim/.config/nvim/plugin/luasnip.lua
@@ -11,18 +11,29 @@ local dynamic = ls.dynamic_node
 ls.add_snippets(nil, {
   cpp = {
     snip({
-      trig = 'include',
+      trig = 'std',
       }, {
-        node(1, {
-          text('#include <'),
-          insert(1),
-          text('>'),
-        }),
-        node(1, {
-          text('using namespace std;'),
-        }),
-      }
-    ),
+      text({'#include <bits/stdc++.h>', 'using namespace std;', ''}),
+      insert(0),
+    }),
+    snip({
+      trig = 'for',
+      }, {
+      text('for (int i = '),
+      insert(1),
+      text('; i < '),
+      insert(2),
+      text('; i++) {'),
+      insert(0),
+    }),
+    snip({
+      trig = 'll',
+      }, {
+      text('long long '),
+      insert(1),
+      text(';'),
+      insert(0),
+    }),
   },
 })
 
