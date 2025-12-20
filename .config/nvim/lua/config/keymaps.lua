bd1a76780425c313146c3d56244f58f6fefc50ea
sserada <serada.dev@gmail.com>
2025-12-21 04:33:21 +0900

    chore: Add Japanese comments for clarity in keymaps.lua configuration

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,8 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 local term_opt = { silent = true }
 
--- esc
+-- Escキーのキーマッピング
+-- インサートモードで jk または kj を入力するとEscキーとして動作
 keymap('i', 'jk', '<esc>', term_opt)
 keymap('i', 'kj', '<esc>', term_opt)
 
