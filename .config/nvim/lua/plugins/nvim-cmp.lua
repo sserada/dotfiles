491acc1663aa051a40a6d6bc984d187220c69e11
sserada <serada.dev@gmail.com>
2025-12-21 04:30:22 +0900

    chore: Add comments for clarity in nvim-cmp.lua configuration

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,51 +1,57 @@
 return {
-  "hrsh7th/nvim-cmp",
+  "hrsh7th/nvim-cmp",                                       -- オートコンプリートプラグインの本体
   lazy = false,
   dependencies = {
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-git",
+    "neovim/nvim-lspconfig",                                -- LSP設定
+    "hrsh7th/cmp-nvim-lsp",                                 -- LSPソース
+    "hrsh7th/cmp-buffer",                                   -- バッファ内テキストのソース
+    "hrsh7th/cmp-path",                                     -- ファイルパスのソース
+    "hrsh7th/cmp-cmdline",                                  -- コマンドラインのソース
+    "hrsh7th/cmp-git",                                      -- Gitのソース
   },
   config = function()
     local cmp = require'cmp'
     cmp.setup({
+      -- キーマッピングの設定
       mapping = cmp.mapping.preset.insert({
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-h>'] = cmp.mapping.abort(),
-        ['<C-l>'] = cmp.mapping.confirm({ select = true }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-j>'] = cmp.mapping.select_next_item(),         -- 次の候補を選択
+        ['<C-k>'] = cmp.mapping.select_prev_item(),         -- 前の候補を選択
+        ['<C-h>'] = cmp.mapping.abort(),                    -- 補完を中断
+        ['<C-l>'] = cmp.mapping.confirm({ select = true }), -- 候補を確定
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),  -- 候補を確定
       }),
+      -- 補完ソースの設定
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp' },                              -- LSPからの補完
       }, {
-        { name = 'buffer' },
+        { name = 'buffer' },                                -- 現在のバッファからの補完
       })
     })
+    -- gitcommitファイルタイプ用の設定
     cmp.setup.filetype('gitcommit', {
       sources = cmp.config.sources({
-        { name = 'cmp_git' },
+        { name = 'cmp_git' },                               -- Gitのコミット履歴からの補完
       }, {
         { name = 'buffer' },
       })
     })
+    -- コマンドライン（検索）用の設定
     cmp.setup.cmdline('/', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = {
         { name = 'buffer' }
       }
     })
+    -- コマンドライン（コマンド）用の設定
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
-        { name = 'path' }
+        { name = 'path' }                                     -- パスからの補完
       }, {
-        { name = 'cmdline' }
+        { name = 'cmdline' }                                  -- コマンドラインからの補完
       })
     })
+    -- LSPのcapabilities設定
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require('lspconfig')['pyright'].setup {
       capabilities = capabilities
