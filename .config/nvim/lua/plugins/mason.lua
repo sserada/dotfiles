846ad948af8b1bc433f5e0132efd05ba19174436
sserada <serada.dev@gmail.com>
2025-12-21 04:19:02 +0900

    chore: Add comments to mason.lua for better clarity on plugin functionality

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,10 +1,10 @@
 return {
-  "williamboman/mason.nvim",
+  "williamboman/mason.nvim",                          -- 外部ツールのインストールと管理用プラグイン
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
+    "williamboman/mason-lspconfig.nvim",              -- mason.nvimとnvim-lspconfigの連携用プラグイン
+    "WhoIsSethDaniel/mason-tool-installer.nvim",      -- mason.nvimで外部ツールを自動インストールするためのプラグイン },
   config = function()
+    -- Masonのセットアップ
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
@@ -12,14 +12,14 @@ return {
     mason.setup({
       ui = {
         icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
+          package_installed = "✓",                    -- インストール済みのパッケージのアイコン
+          package_pending = "➜",                      -- インストール中のパッケージのアイコン
+          package_uninstalled = "✗",                  -- 未インストールのパッケージのアイコン
         },
       },
     })
 
-    mason_lspconfig.setup({
+    mason_lspconfig.setup({                           -- nvim-lspconfig用のLSPサーバーを自動インストール
       ensure_installed = {
         "tsserver",
         "html",
@@ -34,7 +34,7 @@ return {
       },
     })
 
-    mason_tool_installer.setup({
+    mason_tool_installer.setup({                       -- 外部ツールを自動インストール
       ensure_installed = {
         "prettier",
         "stylua",
